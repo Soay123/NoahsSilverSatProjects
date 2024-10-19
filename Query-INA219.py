6379ac3c29bec806dd5a0edc4d88edcd28d7f731
Noah <noah.k.meier@gmail.com>
2024-10-18 22:21:45 -0400

    Add header imports to get started

---
+++ Query-INA219.py
@@ -0,0 +1,11 @@
+#!/Library/Frameworks/Python.framework/Versions/3.12/bin/python3
+import os
+import time
+import board
+import busio
+# import adafruit_ads1x15.ads1015 as ADS
+# from adafruit_ads1x15.analog_in import AnalogIn
+from decimal import Decimal, getcontext
+from adafruit_ina219 import ADCResolution, BusVoltageRange, INA219, Gain, Mode
+
+# Write code below this line
